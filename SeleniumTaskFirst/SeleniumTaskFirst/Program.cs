b1f557211a79c52a46e6cfd0762c9f82adf4d5fc
Denis <denis.milic2@gmail.com>
2021-10-22 22:40:02 +0200

    initial NUnit test implementation

--- SeleniumTaskFirst/SeleniumTaskFirst/Program.cs
+++ SeleniumTaskFirst/SeleniumTaskFirst/Program.cs
@@ -1,4 +1,5 @@
-﻿using OpenQA.Selenium;
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using System;
 
@@ -6,23 +7,37 @@ namespace SeleniumTaskFirst
 {
     class Program
     {
-        static void Main(string[] args)
-        {
-            //Browser Refrence
-            IWebDriver driver = new ChromeDriver();
+        //Browser Refrence
+        IWebDriver driver = new ChromeDriver();
 
+        [SetUp]
+        public void Initilize()
+        {
             //Naviagte to Google
             driver.Navigate().GoToUrl("http://google.com");
+            Console.WriteLine("Navigated to URL");
+        }
 
+        [Test]
+        public void ExecuteTest()
+        {
             //Find cookie confirm button
             IWebElement confirmButton = driver.FindElement(By.Id("L2AGLb"));
             //Click cookie confirm button
             confirmButton?.Click();
+            Console.WriteLine("Confirmed cookie button");
 
             //Find search bar
             IWebElement element = driver.FindElement(By.Name("q"));
-
+            //Send text to search bar
             element.SendKeys("This is Selenium test");
+            Console.WriteLine("Text entered in search bar");
+        }
+
+        [TearDown]
+        public void CleanUp()
+        {
+            driver.Close();
         }
     }
 }
