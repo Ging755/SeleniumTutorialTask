a718f1c3612daf6124a9cad9d1bd9f439217c5fa
Denis <denis.milic2@gmail.com>
2021-10-22 22:18:34 +0200

    Cookie confirmation button and search bar

--- SeleniumTaskFirst/SeleniumTaskFirst/Program.cs
+++ SeleniumTaskFirst/SeleniumTaskFirst/Program.cs
@@ -11,7 +11,18 @@ namespace SeleniumTaskFirst
             //Browser Refrence
             IWebDriver driver = new ChromeDriver();
 
+            //Naviagte to Google
             driver.Navigate().GoToUrl("http://google.com");
+
+            //Find cookie confirm button
+            IWebElement confirmButton = driver.FindElement(By.Id("L2AGLb"));
+            //Click cookie confirm button
+            confirmButton?.Click();
+
+            //Find search bar
+            IWebElement element = driver.FindElement(By.Name("q"));
+
+            element.SendKeys("This is Selenium test");
         }
     }
 }
